e59a456f835ec169212fbc461323a24eb62425f2
JEBASS <JEBASBS@GMAIL.COM>
2022-02-04 17:56:02 -0300

    Salvando datas no banco de dados - #20

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -17,6 +17,10 @@
             <input type="text" class="form-control" name="title" id="title" placeholder="Nome do Evento">
         </div>
         <div class="form-group">
+            <label for="date">Data do evento</label>
+            <input type="date" class="form-control" name="date" id="date">
+        </div>
+        <div class="form-group">
             <label for="title">Cidade</label>
             <input type="text" class="form-control" name="city" id="city" placeholder="Local do Evento">
         </div>
@@ -31,6 +35,25 @@
             <label for="title">Descrição</label>
             <textarea name="description" id="description" class="form-control" placeholder="O que vai acontecer no evento?"></textarea>
         </div>
+        <div class="form-group">
+        <input type="checkbox" name="items[]" value="Cadeiras">
+        </div>
+        <label for="title">Adicione itens de infraestrutura</label>
+            <div class="form-group">
+                <input type="checkbox" name="items[]" value="Cadeiras"> Cadeiras
+            </div>
+            <div class="form-group">
+                <input type="checkbox" name="items[]" value="Palco"> Palco
+            </div>
+            <div class="form-group">
+                <input type="checkbox" name="items[]" value="Cerveja grátis"> Cerveja grátis
+            </div>
+            <div class="form-group">
+                <input type="checkbox" name="items[]" value="Open Food"> Open Food
+            </div>
+            <div class="form-group">
+                <input type="checkbox" name="items[]" value="Brindes"> Brindes
+            </div>
         <input type="submit" class="btn btn-primary" value="Criar Evento">
     </form>
 </div>
